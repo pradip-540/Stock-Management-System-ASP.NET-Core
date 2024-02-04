78be1472a11a8c084b41d872c968d5b1714a4d00
Pradip <ptharu400@rku.ac.in>
2024-02-04 14:25:34 +0530

    Add project files.

---
+++ Stock Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stock_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
