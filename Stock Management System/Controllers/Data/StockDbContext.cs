99fe61fea8aa19d77eb3c626835c1894a986d86a
Pradip <ptharu400@rku.ac.in>
2024-02-12 22:05:16 +0530

    updated database

---
+++ Stock Management System/Controllers/Data/StockDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stock_Management_System.Models;
+
+namespace Stock_Management_System.Controllers.Data
+{
+    public class StockDbContext : DbContext
+    {
+        public StockDbContext(DbContextOptions<StockDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet properties for your entities
+        public DbSet<View> ViewAllItems { get; set; }
+        // Add other DbSet properties for additional entities if needed
+    }
+}
---
+++ Stock Management System/Migrations/20240204181816_StockManagement.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stock_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class StockManagement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ViewAllItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Product = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Item = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ViewAllItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ViewAllItems");
+        }
+    }
+}
